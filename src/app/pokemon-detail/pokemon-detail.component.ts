b36f2b8dd8175a26d1f29d901e2f0e4e2d106a7d
Hodash26 <florent.bailly62400@gmail.com>
2019-11-08 15:10:36 +0100

    add detail components

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
   styleUrls: ['./pokemon-detail.component.css']
 })
+
+
 export class PokemonDetailComponent implements OnInit {
+  @Input() pokemon: Pokemon;
 
   constructor() { }
 
