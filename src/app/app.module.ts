a29b48654381746d014dfbfd3a78ea63e485e9e1
Hodash26 <florent.bailly62400@gmail.com>
2019-11-08 14:51:12 +0100

    add editor, a list and a bit of CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonsComponent } from './pokemons/pokemons.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonsComponent,
+    PokemonDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-pokemons.ts
@@ -0,0 +1,16 @@
+import { Pokemon } from "./pokemon";
+
+export const POKEMONS: Pokemon[] = [
+  { id: 2, name: "Bulbizarre", type: "Plante / Poison", numb: "N°001" },
+  { id: 2, name: "Herbizarre", type: "Plante / Poison", numb: "N°002" },
+  { id: 3, name: "Florizarre", type: "Plante / Poison", numb: "N°003" },
+  { id: 4, name: "Salamèche", type: "Feu", numb: "N°004" },
+  { id: 5, name: "Reptincel", type: "Feu", numb: "N°005" },
+  { id: 6, name: "Dracaufeu", type: "Feu / Vol", numb: "N°006" },
+  { id: 7, name: "Carapuce", type: "Eau", numb: "N°007" },
+  { id: 8, name: "Carabaffe", type: "Eau", numb: "N°008" },
+  { id: 9, name: "Tortank", type: "Eau", numb: "N°009" },
+  { id: 10, name: "Chenipan", type: "Insecte", numb: "N°010" },
+  { id: 11, name: "Chrysacier", type: "Insecte", numb: "N°011" },
+  { id: 12, name: "Papylusion", type: "Insete / Vol", numb: "N°012" }
+];
